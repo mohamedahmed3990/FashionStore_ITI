b33a978472a6db773ec01330a0cf310a497278da
Mostafa <Mostafa_3laaa>
2025-04-14 23:00:50 +0200

    DetailsFeature

---
+++ FashionStore.PL/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using FashionStore.BLL.Services.ProductService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FashionStore.PL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet("details/{id}")]
+        public async Task<IActionResult> GetProductDetails(int id)
+        {
+            var product = await _productService.GetProductDetailsAsync(id);
+            if (product == null) return NotFound();
+
+            return Ok(product);
+        }
+    }
+}
